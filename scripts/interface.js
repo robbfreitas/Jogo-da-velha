f52da1eb7688c2e523b9873dd7eeb6491202591d
RobbFreitas <robertocompromissos@gmail.com>
2021-07-30 22:29:26 -0300

    Finalizada regras essenciais de interface

--- scripts/interface.js
+++ scripts/interface.js
@@ -15,21 +15,34 @@ function handleClick(event) {
     let square = event.target;
     let position = square.id;
 
-    handleMove(position);
-    updateSquares();
+    if (handleMove(position)) {
+
+        setTimeout(() => {
+            alert("Fim de Jogo! - E o vencendor foi o jogador: " + playerTime);
+        }, 10);
+
+    };
+    updateSquares(position);
 }
 
-function updateSquares() {
+function updateSquares(position) {
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class="${symbol}"></div>`
 
-    let squares = document.querySelectorAll(".square");
-
-    squares.forEach((square)=>{
-
-        let position = square.id;
-        let symbol = board[position];
-
-        if(symbol != ""){
-            square.innerHTML = `<div class="${symbol}"></div>`
-        }
-    })
 }
+
+// function updateSquares() {
+
+//     let squares = document.querySelectorAll(".square");
+
+//     squares.forEach((square) => {
+//         let position = square.id;
+//         let symbol = board[position];
+
+//         if (symbol != "") {
+//             square.innerHTML = `<div class= "${symbol}></div>`
+//         }
+//     })
+// }
+
